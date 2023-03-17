b5764bb5e1fe5983c45305e887b3cfe164cb39f4
MunkushiR <ruth.munkushi@student.moringaschool.com>
2023-03-17 12:57:09 +0300

    done

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function deleteFromEmployeeByKey(employee, key) {
   // this function should take in a employee Object and a key. It should delete the key/value pair for the key that was passed in from the employee Object. This should all not actually mutate the employee passed in.
   const NEW_OBJ =  Object.assign({}, employee);
   delete NEW_OBJ[key];
-  return NEW_OBJ;
+  return NEW_OBJ;gi
 }
 
 function destructivelyDeleteFromEmployeeByKey(employee, key) {
